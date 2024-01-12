e26bf99bf187bb2b1c1457feb9995c9ecfe2276a
DonatasNoreika <donoras@gmail.com>
2024-01-12 10:23:30 +0200

    +Vieno įrašo puslapis

--- mysite/myblog/urls.py
+++ mysite/myblog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import PostListView
+from .views import PostListView, PostDetailView
 
 urlpatterns = [
     path("", PostListView.as_view(), name="posts"),
+    path("posts/<int:pk>", PostDetailView.as_view(), name="post"),
 ]
--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Post, Comment
 
+
 # Create your views here.
 class PostListView(generic.ListView):
     model = Post
     template_name = "posts.html"
     context_object_name = "posts"
+
+
+class PostDetailView(generic.DetailView):
+    model = Post
+    template_name = "post.html"
+    context_object_name = 'post'
