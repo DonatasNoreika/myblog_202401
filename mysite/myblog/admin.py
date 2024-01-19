75f86d48f7766b92e7e031eb78068282efcf6647
DonatasNoreika <donoras@gmail.com>
2024-01-19 08:36:02 +0200

    Pridėta postų paieška

--- mysite/myblog/admin.py
+++ mysite/myblog/admin.py
@@ -6,9 +6,11 @@ class CommentInLine(admin.TabularInline):
     model = Comment
     extra = 0
 
+
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'date']
     inlines = [CommentInLine]
+
 
 # Register your models here.
 admin.site.register(Post, PostAdmin)
--- mysite/myblog/urls.py
+++ mysite/myblog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, search
 
 urlpatterns = [
     path("", PostListView.as_view(), name="posts"),
     path("posts/<int:pk>", PostDetailView.as_view(), name="post"),
+    path("search/", search, name="search"),
 ]
