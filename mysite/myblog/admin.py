4e05912b3cf7748b4e2e04fe9a422b0a4da07844
DonatasNoreika <donoras@gmail.com>
2024-01-25 08:19:50 +0200

    Registruojant vartotojo, automatiškai sukuriamas jo profilis su default.png nuotrauka

--- mysite/myblog/admin.py
+++ mysite/myblog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Comment, PostPhoto
+from .models import Post, Comment, PostPhoto, Profile
 
 
 class CommentInLine(admin.TabularInline):
@@ -17,4 +17,4 @@ class PostAdmin(admin.ModelAdmin):
 
 # Register your models here.
 admin.site.register(Post, PostAdmin)
-# admin.site.register(Comment)
+admin.site.register(Profile)
