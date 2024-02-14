5627bf9b4d2155bbf0de6e999d7e7e7c9981a673
DonatasNoreika <donoras@gmail.com>
2024-02-14 09:20:36 +0200

    Paruoštas serveriui3

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = False
-ALLOWED_HOSTS = ['192.168.8.155']
+ALLOWED_HOSTS = ['176.223.138.148']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
